d758a48b86f40d83ae7b4bbc8a627d942053fc6e
cohji <koji.suzuki.cc@gmail.com>
2018-08-27 13:03:27 +0900

    Initial commit

---
+++ platforms/android/app/build/intermediates/assets/debug/www/js/index.js
@@ -0,0 +1,61 @@
+function onDeviceReady() {
+    // 必要な権限を取得
+    // 通知の権限
+    cordova.plugins.notification.local.requestPermission(function () {
+        console.log("Permitted.")
+
+    });
+    // 位置情報を取得。常に取得とします。
+    cordova.plugins.locationManager.requestAlwaysAuthorization();
+    
+    // delegateの作成と設定
+    var delegate = new cordova.plugins.locationManager.Delegate();
+    delegate.didDetermineStateForRegion = function(pluginResult) {
+        console.log('didDetermineStateForRegion', pluginResult);
+    }
+    delegate.didStartMonitoringForRegion = function(pluginResult) {
+        console.log('didStartMonitoringForRegion', pluginResult);
+    }
+    
+    delegate.didRangeBeaconsInRegion = function(pluginResult) {
+        console.log('didRangeBeaconsInRegion', pluginResult);
+    }
+    
+    delegate.didEnterRegion = function(pluginResult) {
+        console.log('didEnterRegion', pluginResult);
+        cordova.plugins.notification.local.schedule({
+            id: 101,
+            title: "いらっしゃいませ",
+            text: "アプリを立ち上げてクーポンをゲットしてください",
+            trigger: {at: new Date(new Date().getTime() + (1 * 1000))}
+        });
+    }
+    delegate.didExitRegion = function(pluginResult) {
+        console.log('didExitRegion', pluginResult);
+        cordova.plugins.notification.local.schedule({
+            id: 100,
+            title: "ご来店ありがとうございました",
+            text: "またのお越しを楽しみにしております。",
+            trigger: {at: new Date(new Date().getTime() + (1 * 1000))}
+        });
+    }
+    
+    cordova.plugins.locationManager.setDelegate(delegate);
+    
+    // 監視するビーコンの作成
+    var uuid = 'B9407F30-F5F8-466E-AFF9-25556B57FE6D'; //ビーコンのUUID
+    var identifier = 'shop001'; // 任意のID
+    var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid);
+    beaconRegion.notifyEntryStateOnDisplay = true;
+
+    // 監視の開始
+    // cordova.plugins.locationManager.startRangingBeaconsInRegion(beaconRegion)
+    //     .fail(console.error)
+    //     .done();
+        
+    cordova.plugins.locationManager.startMonitoringForRegion(beaconRegion)
+        .fail(function(e) { console.error(e); })
+        .done();
+}
+
+document.addEventListener('deviceready', onDeviceReady, false);
